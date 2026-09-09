77ba1423662c206600d431b4eb8ef1f0fb17bee3
realazaber <azcooldude8@gmail.com>
2026-09-09 18:53:31 +0200

    Refactor: Use route() named routes everywhere (welcome blade)

--- blogravel/resources/views/welcome.blade.php
+++ blogravel/resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <div class="text-center">
             <h1 class="text-4xl font-bold text-gray-900">{{ config('app.name', 'Blogravel') }}</h1>
             <p class="mt-2 text-gray-600">A self-hosted blogging platform.</p>
-            <a href="/admin" class="inline-block mt-6 px-6 py-2 bg-gray-900 text-white rounded-lg hover:bg-gray-800">Admin</a>
+            <a href="{{ route('filament.admin.pages.dashboard') }}" class="inline-block mt-6 px-6 py-2 bg-gray-900 text-white rounded-lg hover:bg-gray-800">Admin</a>
         </div>
     </body>
 </html>

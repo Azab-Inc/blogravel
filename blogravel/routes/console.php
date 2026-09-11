13333ffd4b863c3aeec7b4da253c20b70263d221
realazaber <azcooldude8@gmail.com>
2026-09-11 13:55:36 +0200

    feat: register scheduled backup task

--- blogravel/routes/console.php
+++ blogravel/routes/console.php
@@ -1,8 +1,25 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+use App\Jobs\CreateBackupJob;
+use App\Models\BackupRule;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    $now = now();
+    $rules = BackupRule::where('enabled', true)
+        ->where(function ($query) use ($now) {
+            $query->whereNull('next_run_at')
+                ->orWhere('next_run_at', '<=', $now);
+        })
+        ->get();
+
+    foreach ($rules as $rule) {
+        CreateBackupJob::dispatch($rule->id);
+        $rule->update(['next_run_at' => $now->copy()->addHour()]);
+    }
+})->everyFifteenMinutes()->withoutOverlapping();

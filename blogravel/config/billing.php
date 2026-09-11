a084ff7b7ff2b24e80c85bf9a26ca1ffea05cea9
realazaber <azcooldude8@gmail.com>
2026-09-11 13:49:19 +0200

    feat: add backup retention and size limits to billing config

--- blogravel/config/billing.php
+++ blogravel/config/billing.php
@@ -39,16 +39,22 @@ return [
             'posts' => 50,
             'max_image_size_mb' => 2,
             'users' => 3,
+            'backup_retention_days' => env('BACKUP_FREE_RETENTION_DAYS', 30),
+            'backup_max_size_mb' => env('BACKUP_FREE_MAX_SIZE_MB', 500),
         ],
         'pro' => [
             'posts' => null,
             'max_image_size_mb' => 10,
             'users' => 10,
+            'backup_retention_days' => env('BACKUP_PRO_RETENTION_DAYS', 120),
+            'backup_max_size_mb' => env('BACKUP_PRO_MAX_SIZE_MB', 2000),
         ],
         'business' => [
             'posts' => null,
             'max_image_size_mb' => 25,
             'users' => null,
+            'backup_retention_days' => env('BACKUP_BUSINESS_RETENTION_DAYS', 120),
+            'backup_max_size_mb' => env('BACKUP_BUSINESS_MAX_SIZE_MB', 5000),
         ],
     ],
 
